d8c110fe555e132f82aad53f1eb55d2f68f76e44
Oleksii Kutsenko <kutsenko.new@gmail.com>
2023-08-16 22:48:13 +0200

    Mobs have been added

--- src/enemies/MobGenerator.java
+++ src/enemies/MobGenerator.java
@@ -1,9 +1,42 @@
 package enemies;
 
 public class MobGenerator {
-    private static final Mob[] possibleMobs = {
-            new Mob("Goblin", 10, 5),
-            new Mob("Orc", 20, 10),
-            new Mob("Troll", 30, 15)
-    };
+    private static final String[] mobNames = {"Goblin", "Orc", "Skeleton", "Wolf", "Spider"};
+
+    public static Mob generateRandomMob() {
+        String randomName = getRandomMobName();
+        int baseHP = 0;
+        int baseAttack = 0;
+
+        switch (randomName) {
+            case "Goblin" -> {
+                baseHP = 10;
+                baseAttack = 10;
+            }
+            case "Orc" -> {
+                baseHP = 20;
+                baseAttack = 15;
+            }
+            case "Skeleton" -> {
+                baseHP = 15;
+                baseAttack = 20;
+            }
+            case "Wolf" -> {
+                baseHP = 10;
+                baseAttack = 25;
+            }
+            case "Spider" -> {
+                baseHP = 5;
+                baseAttack = 30;
+            }
+        }
+
+        return new Mob(randomName, baseHP, baseAttack);
+    }
+
+
+    private static String getRandomMobName() {
+        return mobNames[(int) (Math.random() * mobNames.length)];
+    }
+
 }
--- src/hero/Hero.java
+++ src/hero/Hero.java
@@ -4,13 +4,19 @@ import attributes.HeroAttributes;
 
 public class Hero {
     private final HeroAttributes heroAttributes;
+    private int HP;
 
     public Hero(HeroType heroType) {
         this.heroAttributes = HeroFactory.createHeroAttributes(heroType);
+        this.HP = heroAttributes.getHP();
     }
 
     public int getHP() {
-        return heroAttributes.getHP();
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
     }
 
     public int getAttackPower() {
